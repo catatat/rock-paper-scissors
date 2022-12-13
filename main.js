f0f8868a49ee435fadf10da4c76a00e9d7b3fdb8
Catalina Esqueda <70826064+catatat@users.noreply.github.com>
2022-12-12 19:20:21 -0500

    Create function to randomly select computer's choice in rock, paper, scissors

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice () {
+    let gameOptions = [
+        'Rock',
+        'Paper',
+        'Scissors'
+    ];
+    let randomChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)];
+    return randomChoice;
+}
+console.log(getComputerChoice ());
