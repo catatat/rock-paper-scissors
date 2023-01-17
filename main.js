a27fb49ecc400ea3d5f0b68dbee71a2f1c076df7
Catalina Esqueda <70826064+catatat@users.noreply.github.com>
2023-01-17 18:40:58 -0500

    Create function that plays a single round of Rock Paper Scissors

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let gameOptions = [
         'Rock',
         'Paper',
@@ -7,4 +7,36 @@ function getComputerChoice () {
     let randomChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)];
     return randomChoice;
 }
-console.log(getComputerChoice ());
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            return "It's a tie!";
+        } else if (computerSelection == "Paper") {
+            return "You lose! Paper beats rock.";
+        } else {
+            return "You win! Rock beats scissors.";
+        }
+    } else if (playerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            return "You win! Paper beats rock.";
+        } else if (computerSelection == "Paper") {
+            return "It's a tie!";
+        } else {
+            return "You lose! Scissors beat paper.";
+        }
+    } else if (playerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+            return "You lose! Rock beats scissors";
+        }
+        else if (computerSelection == "Paper") {
+            return "You win! Scissors beat paper.";
+        }
+        else {
+            return "It's a tie!";
+        }
+    } else {
+        return "You must choose rock, paper, or scissors.";
+    }
+}
