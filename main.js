07ef3b5bf50c17bed54857ab9434d2a82c925c75
Catalina Esqueda <70826064+catatat@users.noreply.github.com>
2023-01-20 20:00:36 -0500

    Add ability to track game score

--- main.js
+++ main.js
@@ -8,34 +8,47 @@ function getComputerChoice() {
     return randomSelection;
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound() {
 
     let playerSelection = prompt("Choose rock, paper, or scissors");
 
     let computerSelection = getComputerChoice();
 
+    let playerWin;
+
+    let computerWin;
+
     if (playerSelection.toLowerCase() == "rock") {
         if (computerSelection == "Rock") {
             alert("It's a tie!");
         } else if (computerSelection == "Paper") {
             alert("You lose! Paper beats rock.");
+            return ++computerScore;
         } else {
             alert("You win! Rock beats scissors.");
+            return ++playerScore;
         }
     } else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection == "Rock") {
             alert("You win! Paper beats rock.");
+            return ++playerScore;
         } else if (computerSelection == "Paper") {
             alert("It's a tie!");
         } else {
             alert("You lose! Scissors beat paper.");
+            return ++computerScore;
         }
     } else if (playerSelection.toLowerCase() == "scissors") {
         if (computerSelection == "Rock") {
             alert("You lose! Rock beats scissors");
+            return ++computerScore;
         }
         else if (computerSelection == "Paper") {
             alert("You win! Scissors beat paper.");
+            return ++playerScore;
         }
         else {
             alert("It's a tie!");
@@ -47,11 +60,9 @@ function playRound() {
 }
 
 function game () {
-    let playerScore = 0;
-    let computerScore = 0;
-
     for (let i = 0; i < 5; i++) { 
         playRound();
+        alert(`Player: ${playerScore}. Computer: ${computerScore}`);
     }
 }
 
